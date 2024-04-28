3b80ad0d4e125f47051f90e1e0d0b234736dc7d0
MelchiorDahrk <47513579+MelchiorDahrk@users.noreply.github.com>
2024-04-28 15:57:25 -0400

    Updated tooltips

--- 01 MWSE Add-Ons/MWSE/mods/md/24/TooltipsComplete/main.lua
+++ 01 MWSE Add-Ons/MWSE/mods/md/24/TooltipsComplete/main.lua
@@ -1,10 +1,14 @@
 local tooltipsComplete = include("Tooltips Complete.interop")
 local tooltipData = {
     { id = "md24_inscguarhide", description = "This ancient guar hide is inscribed with esoteric symbols.", itemType = "miscItem" },
-    { id = "md24_c_AshbaneGirdle", description = "Crafted from supple leather and dyed in the fiery hues of sunset, the belt exudes a faint warmth reminiscent of the fires of Red Mountain itself.", itemType = "clothing" }
-	{ id = "md24_c_StoneOfGrounding", description = "The stone's surface is etched with scars from repeated lightning strikes.", itemType = "clothing" }
-	{ id = "md24_c_TheTwelfthTalisman", description = "A large, aquamarine stone sits in the center of the talisman. Colorful shells woven together by leather straps seem to whisper tales of forgotten shores if you listen closely.", itemType = "clothing" }
-	{ id = "md24_c_TheWhirlingband", description = "A sleek, ebony ring adorned with swirling glyphs that seem to dance and writhe like tendrils of smoke. When worn, the ring emits a faint hum, as if echoing the wind itself.", itemType = "clothing" }
+    { id = "md24_c_AshbaneGirdle", description = "Crafted from supple leather and dyed in the fiery hues of sunset, the belt exudes a faint warmth reminiscent of the fires of Red Mountain itself.", itemType = "clothing" },
+	{ id = "md24_c_StoneOfGrounding", description = "The stone's surface is etched with scars from repeated lightning strikes.", itemType = "clothing" },
+	{ id = "md24_c_TheTwelfthTalisman", description = "A large, aquamarine stone sits in the center of the talisman. Colorful shells woven together by leather straps seem to whisper tales of forgotten shores if you listen closely.", itemType = "clothing" },
+	{ id = "md24_c_TheWhirlingband", description = "A sleek, ebony ring adorned with swirling glyphs that seem to dance and writhe like tendrils of smoke. When worn, the ring emits a faint hum, as if echoing the wind itself.", itemType = "clothing" },
+    { id = "md24_IngCrea_MoonJelly", description = "This glowing, gelatinous substance comes from the juvenile life stage of the netch which are commonly known as 'Moon Jellies'.", itemType = "ingredient" },
+    { id = "md24_IngFlor_LotusBlood", description = "The rare Blood Lotus flower represents 'rebirth through death' to the Ashlander tribes of Morrowind.", itemType = "ingredient" },
+    { id = "md24_clumsy_spear", description = "The unusually spiny tip of this chitin spear is mystically enchanted to sap the mobility from a foe.", itemType = "weapon" },
+    { id = "md24_sureflight_bow", description = "This high quality chitin bow was crafted by an Erabenimsun warrior and is perfect for hunting game.", itemType = "weapon" },
 }
 local function initialized()
     if tooltipsComplete then
--- merge_to_master.bat
+++ merge_to_master.bat
@@ -1,3 +1,3 @@
 @echo off
-merge_to_master %1 "Red Wisdom.esm" --overwrite --remove-deleted
+merge_to_master %1 "00 Core\\Red Wisdom.esm" --overwrite --remove-deleted
 pause
